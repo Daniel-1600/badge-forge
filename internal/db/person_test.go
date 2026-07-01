ca1a7bb8657b04066812fc0577769cb82a97b54f
Daniel Mungai <chegedan699@gmail.com>
2026-07-01 03:42:53 +0300

    feat: adding tests coverage

--- internal/db/person_test.go
+++ internal/db/person_test.go
@@ -28,3 +28,34 @@ func TestGetPersonByID(t *testing.T) {
 		db.Exec(`DELETE FROM persons WHERE email = ?`, "test@example.com")
 	})
 }
+
+func TestGetPersons(t *testing.T) {
+
+	db := setupTestDB(t)
+
+	err := db.Exec(`INSERT INTO persons (nickname, email) VALUES (?, ?), (?, ?), (?, ?)`,
+		"daniel-test", "daniel-test@example.com",
+		"alice-test", "alice-test@example.com",
+		"bob-test", "bob-test@example.com",
+	).Error
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := db.Exec(`DELETE FROM persons WHERE nickname IN (?, ?, ?)`,
+			"daniel-test", "alice-test", "bob-test").Error
+		require.NoError(t, err)
+	})
+
+	persons, err := GetPersons(db, 1, 10)
+	require.NoError(t, err)
+
+	nicknames := make([]string, len(persons))
+	for i, p := range persons {
+		nicknames[i] = p.Nickname
+	}
+
+	require.Contains(t, nicknames, "daniel-test")
+	require.Contains(t, nicknames, "alice-test")
+	require.Contains(t, nicknames, "bob-test")
+
+}
